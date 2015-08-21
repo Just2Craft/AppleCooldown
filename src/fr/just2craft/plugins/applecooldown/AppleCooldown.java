a3b89a35afa3bf8c7df89216e4d9545f2c37bc8e
SpyL1nk <exo.war@gmail.com>
2015-08-21 17:12:18 +0200

    Ajout d'un commentaire

--- src/fr/just2craft/plugins/applecooldown/AppleCooldown.java
+++ src/fr/just2craft/plugins/applecooldown/AppleCooldown.java
@@ -8,10 +8,11 @@ public class AppleCooldown extends JavaPlugin {
 	public void onEnable() {
 		saveDefaultConfig();
 		Bukkit.getServer().getPluginManager().registerEvents(new AppleListener(), this);
-		Bukkit.getServer().getLogger().info("[AppleCooldownV2] Le plugin est activé !");
+		Bukkit.getServer().getLogger().info("[AppleCooldown] Le plugin est activé !");
 	}
 
 	public void onDisable() {
-		Bukkit.getServer().getLogger().info("AppleCooldownV2 désactivé !");
+		//Il fraudrai vider le HashMap ainsi que les ScheduledTask du plugin avant la désactivation
+		Bukkit.getServer().getLogger().info("[AppleCooldown] désactivé !");
 	}
 }
